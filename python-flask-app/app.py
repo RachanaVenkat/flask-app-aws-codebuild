68351862c208ad363303c7f7fc0f59013bce4a98
Rachana <151712438+RachanaVenkat@users.noreply.github.com>
2024-05-25 19:50:55 +0530

    Update app.py

--- python-flask-app/app.py
+++ python-flask-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! Now testing'
+    return 'Hello, world! Now testing twice'
 
 if __name__ == '__main__':
     app.run()
